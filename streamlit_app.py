fabbc2bed59660cf650e504bc2e38fe2eb968aea
lwyarborough <125509274+lwyarborough@users.noreply.github.com>
2023-02-15 16:29:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 'Breakfast Menu' 
- 
+'Omega 3 and Blueberry Oatmeal'
+'Kale, spinach and rocket smoothie'
+'Hard-Boiled and Free-Range Egg'
+
